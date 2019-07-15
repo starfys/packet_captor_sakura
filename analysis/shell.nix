0e9531c03811d064c0575845df3679a5cad92b33
Steven <srs6p@mtmail.mtsu.edu>
2019-07-15 01:02:13 -0500

    Updated research

--- analysis/shell.nix
+++ analysis/shell.nix
@@ -13,25 +13,53 @@
 # 
 # You should have received a copy of the GNU General Public License
 # along with url_queue.  If not, see <http://www.gnu.org/licenses/>.
+{ pkgs ? import <nixpkgs> {}, pkgs_unstable ? import <nixpkgs-unstable> {}, pythonPackages ? pkgs_unstable.python3Packages }:
 let
-  unstable = import <nixos-unstable> {};
+  kernels = [];
+  additionalExtensions = [
+    "@jupyter-widgets/jupyterlab-manager"
+  ];
 in
-{ pkgs ? import <nixpkgs> {} }:
-  unstable.mkShell {
-    buildInputs = [
-      (master.python37.withPackages (ps: with ps; [
-        pip
-        virtualenv
-        jupyterlab
-        matplotlib
-        numpy
-        pandas
-	pytorchWithoutCuda
-        scikitlearn
-        scipy
-        seaborn
-	tqdm
-        #tensorflowWithoutCuda
-        #tensorflow-tensorboard
-      ]))];
+pkgs.mkShell rec {
+  buildInputs = [
+    # Base Packages
+    pythonPackages.jupyterlab pkgs.nodejs
+  ] ++
+  # Python deps
+  [
+    pythonPackages.matplotlib
+    pythonPackages.numpy
+    pythonPackages.pandas
+    pythonPackages.pytorchWithCuda
+    pythonPackages.ipywidgets
+    pythonPackages.scikitlearn
+    pythonPackages.scipy
+    pythonPackages.seaborn
+    pythonPackages.tqdm
+  ] ++
+  # Kernels
+  kernels;
+
+  shellHook = ''
+    export CUDA_HOME="${pkgs.cudatoolkit}"
+    TEMPDIR=$(mktemp -d -p /tmp)
+    mkdir -p $TEMPDIR
+    cp -r ${pythonPackages.jupyterlab}/share/jupyter/lab/* $TEMPDIR
+    chmod -R 755 $TEMPDIR
+    echo "$TEMPDIR is the app directory"
+
+    # kernels
+    export JUPYTER_PATH="${pkgs.lib.concatMapStringsSep ":" (p: "${p}/share/jupyter/") kernels}"
+
+    # labextensions
+    ${pkgs.stdenv.lib.concatMapStrings
+         (s: "jupyter labextension install --no-build --app-dir=$TEMPDIR ${s}; ")
+         (pkgs.lib.unique
+           ((pkgs.lib.concatMap
+               (d: pkgs.lib.attrByPath ["passthru" "jupyterlabExtensions"] [] d)
+               buildInputs) ++ additionalExtensions))  }
+    jupyter lab build --app-dir=$TEMPDIR
+    # start jupyterlab
+    jupyter lab --app-dir=$TEMPDIR
+  '';
 }

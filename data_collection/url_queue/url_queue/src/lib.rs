7e8a04c310c7359c81ca73c8632e945787e4e935
Steven <srs6p@mtmail.mtsu.edu>
2019-11-25 02:38:33 -0600

    Added lib target

---
+++ data_collection/url_queue/url_queue/src/lib.rs
@@ -0,0 +1,23 @@
+// Copyright 2018 Steven Sheffey
+// This file is part of url_queue.
+//
+// url_queue is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// url_queue is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with url_queue.  If not, see <http://www.gnu.org/licenses/>.
+#![allow(unused_imports)]
+
+pub mod capture;
+pub mod config;
+pub mod service;
+pub mod shutdown;
+pub mod url;
+pub mod work;
